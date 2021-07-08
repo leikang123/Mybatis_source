8557f9aeae49ef0e5be52e847e69bcb6aa8f4b05
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-07-08 17:46:22 +0800

    工具类实现

--- MybatisYuanMa/src/main/java/com/kkb/mybatis/framework/utils/ParameterMappingTokenHandler.java
+++ MybatisYuanMa/src/main/java/com/kkb/mybatis/framework/utils/ParameterMappingTokenHandler.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.kkb.mybatis.framework.config.ParameterMapping;
-
+// 解析映射器实现工具类
 public class ParameterMappingTokenHandler implements TokenHandler {
+	// 解析映射器排列，可重复，有序列
 	private List<ParameterMapping> parameterMappings = new ArrayList<>();
 
 	// context是参数名称
@@ -19,11 +20,11 @@ public class ParameterMappingTokenHandler implements TokenHandler {
 		ParameterMapping parameterMapping = new ParameterMapping(content);
 		return parameterMapping;
 	}
-
+        // 获取解析映射器
 	public List<ParameterMapping> getParameterMappings() {
 		return parameterMappings;
 	}
-
+       // 设置解析映射器
 	public void setParameterMappings(List<ParameterMapping> parameterMappings) {
 		this.parameterMappings = parameterMappings;
 	}
